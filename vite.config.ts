9df1c5a8eebee47c524d13ff00c6a031ff0c4e8b
stanley <stanleygomesdasilva@gmail.com>
2025-11-05 15:37:40 -0300

    feat: otimizar imagens do site no build

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,13 @@ import { reactRouter } from "@react-router/dev/vite";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
+import { ViteImageOptimizer } from "vite-plugin-image-optimizer";
 
 export default defineConfig({
-  plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
+  plugins: [
+    tailwindcss(),
+    reactRouter(),
+    tsconfigPaths(),
+    ViteImageOptimizer({}),
+  ],
 });
